d9bcb867b3990a2ea87a7bf0278081c709d85562
dalymoustafa <dalymoustafa@gmail.com>
2026-03-09 23:27:46 +0200

    Update fetch-offices.js

--- fetch-offices.js
+++ fetch-offices.js
@@ -167,9 +167,9 @@ async function main() {
           tooltip.style.top = (point.y + 16) + 'px';
         });
       });
-      map.setView([20, -20], 2);
+      map.setView([20, -20], 1);
     });
-  map.setView([20, -20], 2);
+  map.setView([20, -20], 1);
 <\/script>
 </body>
 </html>`;
